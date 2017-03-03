c3c0d7cb1d6286a4d70a715967610ff75dd8aaa2
Dorian Savina <d.savina@thecodingmachine.com>
2017-03-03 20:57:20 +0100

    added parser JsonEncoder
    Signed-off-by: Dorian Savina <d.savina@thecodingmachine.com>

---
+++ src/Parser/JsonEncoder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace FormHandler\Parser;
+
+use FormHandler\Exception\ParsingException;
+
+/**
+ * Class JsonEncoder
+ * @package FormHandler\Parser
+ */
+class JsonEncoder implements ParserInterface
+{
+    /** @var int */
+    private $options;
+
+    /**
+     * JsonParser constructor.
+     * @param int $options
+     */
+    public function __construct($options = 0)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @param $rawValue
+     * @return string
+     *
+     * @throws ParsingException
+     */
+    public function parse($rawValue)
+    {
+        $json = json_encode($rawValue, $this->options);
+        if ($json === false) {
+            throw new ParsingException(json_last_error_msg());
+        } else {
+            return $json;
+        }
+    }
+}
